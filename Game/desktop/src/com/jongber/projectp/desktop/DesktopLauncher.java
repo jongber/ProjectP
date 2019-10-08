cddecc90e89c8db4c4158cf67aacff1716812703
JongBeom An <jongber@gmail.com>
2019-10-08 17:37:46 +0900

    Source summit

---
+++ Game/desktop/src/com/jongber/projectp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jongber.projectp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jongber.projectp.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
