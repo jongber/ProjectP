aa9396bd32cde8b1451e7f1251aad64c3e4ac0f3
JongBeom An <jongber@gmail.com>
2019-10-09 16:22:16 +0900

    test codes submit

--- Game/desktop/src/com/jongber/projectp/desktop/DesktopLauncher.java
+++ Game/desktop/src/com/jongber/projectp/desktop/DesktopLauncher.java
@@ -3,10 +3,19 @@ package com.jongber.projectp.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.jongber.projectp.MyGdxGame;
+import com.jongber.projectp.TestSpriteAnim;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+
+        config.title = "camera";
+
+
+        config.width = 1280;
+
+        config.height = 720;
+		//new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new TestSpriteAnim(), config);
 	}
 }
