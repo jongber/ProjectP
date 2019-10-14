de39b5704c7390b9eee41fb460cb34d77a05f3a6
JongBeom An <jongber@gmail.com>
2019-10-14 21:06:02 +0900

    static texture loader 작성

--- Game/core/src/com/jongber/projectp/asset/AssetLoader.java
+++ Game/core/src/com/jongber/projectp/asset/AssetLoader.java
@@ -8,11 +8,29 @@ import com.jongber.projectp.asset.aseprite.Frame;
 import com.jongber.projectp.asset.aseprite.FrameTag;
 import com.jongber.projectp.asset.aseprite.Layer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AssetLoader {
+
+    public static StaticTextureAsset loadTexture(String name, AsepriteJson json) {
+        StaticTextureAsset asset = new StaticTextureAsset();
+
+        Texture texture = new Texture((json.getImgPath()));
+        asset.set(name, texture);
+
+        //// *set pivot point
+        //// y좌표축 방향이 다름..
+        int height = texture.getHeight();
+        for (Layer layer : json.meta.layers) {
+            if (layer.name.startsWith("P_pivot")) {
+                String[] split = layer.name.split("_");
+                asset.setPivot(new Vector2(Integer.parseInt(split[2]), height - Integer.parseInt(split[3])));
+            }
+        }
+
+        return asset;
+    }
 
     public static SpriteAsset loadSprite(String name, AsepriteJson json) {
 
--- Game/core/src/com/jongber/projectp/asset/StaticTextureAsset.java
+++ Game/core/src/com/jongber/projectp/asset/StaticTextureAsset.java
@@ -1,11 +1,37 @@
 package com.jongber.projectp.asset;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 public class StaticTextureAsset {
+    private String name;
     private Texture texture;
+    private Vector2 pivot = new Vector2();
 
+    public void set(String name, Texture texture) {
+        this.name = name;
+        this.texture = texture;
+    }
 
+    public void setPivot(Vector2 pivot) {
+        this.pivot = pivot;
+    }
+
+    public int getPivotX() {
+        return (int)this.pivot.x;
+    }
+
+    public int getPivotY() {
+        return (int)this.pivot.y;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Texture getTexture() {
+        return this.texture;
+    }
 
     public void dispose() {
         this.texture.dispose();
