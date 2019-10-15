fcffd079f04ae487d5f7725e3a55f8693dc98be8
JongBeom An <jongber@gmail.com>
2019-10-15 22:23:50 +0900

    game setting json 추가

---
+++ Game/core/src/com/jongber/projectp/asset/GameSettingJson.java
@@ -0,0 +1,32 @@
+package com.jongber.projectp.asset;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jongber.projectp.asset.aseprite.wh;
+
+import java.io.IOException;
+
+public class GameSettingJson {
+    public wh screenSize;
+
+    public static GameSettingJson load() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            FileHandle file = Gdx.files.internal("gamesetting.json");
+            String json = file.readString();
+
+            GameSettingJson settingJson = mapper.readValue(json, GameSettingJson.class);
+            Gdx.app.log("DEBUG", "gamesetting json loaded");
+
+            return settingJson;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        return null;
+    }
+}
