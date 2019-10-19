8ea71a22d8f5e9b05677691c9750ecdb8ec05750
JongBeom An <jongber@gmail.com>
2019-10-19 18:50:14 +0900

    컴포넌트 수정

--- Game/core/src/com/jongber/projectp/object/component/GameLoopComponent.java
+++ Game/core/src/com/jongber/projectp/object/component/GameLoopComponent.java
@@ -1,17 +1,22 @@
 package com.jongber.projectp.object.component;
 
+import com.jongber.projectp.object.GameObject;
+
 public class GameLoopComponent {
+
     public interface LoopRunner {
-        void update();
+        void update(GameObject owner);
     }
 
+    private GameObject owner;
     private LoopRunner loopRunner;
 
-    public GameLoopComponent(LoopRunner loopRunner) {
+    public GameLoopComponent(GameObject owner, LoopRunner loopRunner) {
         this.loopRunner = loopRunner;
+        this.owner = owner;
     }
 
     public void update() {
-        loopRunner.update();
+        loopRunner.update(this.owner);
     }
 }
