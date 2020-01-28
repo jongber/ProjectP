f6980f224f20ec88aab11f648a18fae786898fed
jongber <jongber@gmail.com>
2020-01-28 15:39:13 +0900

    perf 조정

--- Game2/core/src/com/jongber/game/core/controller/PerfRenderer.java
+++ Game2/core/src/com/jongber/game/core/controller/PerfRenderer.java
@@ -12,7 +12,8 @@ public class PerfRenderer extends Controller implements Controller.Renderer{
     private BitmapFont font;
     private Vector3 fpsPos = new Vector3();
     private float totalElapsed = 0.0f;
-    private float elapsed = 0.0f;
+    private float avgElapsed = 0.016666f;
+    private int renderCall = 0;
 
     public PerfRenderer() {
         font = new BitmapFont();
@@ -30,17 +31,17 @@ public class PerfRenderer extends Controller implements Controller.Renderer{
 
     @Override
     public void render(SpriteBatch batch, OrthoCameraWrapper camera, float elapsed) {
-        if (totalElapsed == 0.0f) {
-            this.elapsed = elapsed;
-        }
+        this.totalElapsed += elapsed;
+        this.renderCall++;
 
-        totalElapsed += elapsed;
-        if (totalElapsed >= 1.0f) {
-            totalElapsed = 0.0f;
+        if (this.totalElapsed >= 1.0f) {
+            this.avgElapsed = this.totalElapsed/this.renderCall;
+            this.totalElapsed = 0;
+            this.renderCall = 0;
         }
 
         this.fpsPos.setZero();
         camera.getCamera().unproject(this.fpsPos);
-        font.draw(batch, "fps:" + (int)(1/this.elapsed), this.fpsPos.x, this.fpsPos.y - this.font.getLineHeight());
+        font.draw(batch, "fps:" + (int)(1/this.avgElapsed), this.fpsPos.x, this.fpsPos.y - this.font.getLineHeight());
     }
 }
