8ce1820f39151f697278d4496bbee1cfe6ba8a23
JongBeom An <jongber@gmail.com>
2020-01-28 23:52:06 +0900

    ui 작성중

--- Game2/desktop/src/com/jongber/game/desktop/room/RoomEditorDialog.java
+++ Game2/desktop/src/com/jongber/game/desktop/room/RoomEditorDialog.java
@@ -7,7 +7,6 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Label;
-import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,9 +15,8 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JSpinner;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -43,10 +41,14 @@ public class RoomEditorDialog {
 
     public static void popRoomUI(GameLayer layer) {
         JDialog dialog = new JDialog();
-        dialog.setSize(200, 400);
+        dialog.setSize(400, 400);
+        //dialog.setResizable(false);
+
+        //JPanel dialog = new JPanel();
         dialog.setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
+        //gbc.insets = new Insets(5,5,0,0);
 
         // 1. room name
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -56,29 +58,79 @@ public class RoomEditorDialog {
 
         gbc.gridx = 1;
         gbc.gridy = 0;
-        TextField nameField = new TextField(10);
+        TextField nameField = new TextField();
         dialog.add(nameField, gbc);
 
         // 2. sanity
         gbc.gridx = 0;
         gbc.gridy = 1;
-        Label sanityLabel = new Label("Sanity 50");
+        Label sanityLabel = new Label("Sanity: 50");
         dialog.add(sanityLabel, gbc);
 
+        gbc.gridx = 1;
+        gbc.gridy = 1;
         JSlider sanitySlider = new JSlider();
-        sanitySlider.setMinorTickSpacing(5);
-        sanitySlider.setMajorTickSpacing(25);
-        sanitySlider.setPaintTicks(true);
-        sanitySlider.setPaintLabels(true);
+        sanitySlider.setMinimum(0);
+        sanitySlider.setMaximum(100);
         sanitySlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent changeEvent) {
+                sanityLabel.setText("Sanity: " + sanitySlider.getValue());
             }
         });
-        gbc.gridx = 1;
-        gbc.gridy = 1;
         dialog.add(sanitySlider, gbc);
 
+        // 3. noise
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        Label noiseLabel = new Label("Noise: 50");
+        dialog.add(noiseLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        JSlider noiseSlider = new JSlider();
+        noiseSlider.setMinimum(0);
+        noiseSlider.setMaximum(100);
+        noiseSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                noiseLabel.setText("Noise: " + noiseSlider.getValue());
+            }
+        });
+        dialog.add(noiseSlider, gbc);
+
+        // 4. height
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        Label heightLabel = new Label("Height: ");
+        dialog.add(heightLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        dialog.add(new Label("3 block(48px) fixed"), gbc);
+
+        // 5. width
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        Label widthLabel = new Label("Width: ");
+        dialog.add(widthLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        JSpinner widthSpinner = new JSpinner();
+        dialog.add(widthSpinner, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        dialog.add(new Label("block"), gbc);
+
+        // 6. wallpaper
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        JButton pathButton = new JButton("wallpaper");
+        dialog.add(pathButton, gbc);
+
+        //dialog.add(dialog);
         dialog.setVisible(true);
     }
 
