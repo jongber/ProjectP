531af5c5390b847bb3e52af55fe36b4af00e311e
jongber <jongber@gmail.com>
2020-02-07 13:36:11 +0900

    prop path changed

--- Game2/desktop/src/com/jongber/game/desktop/map/event/DelObjectEvent.java
+++ Game2/desktop/src/com/jongber/game/desktop/map/event/DelObjectEvent.java
@@ -4,12 +4,12 @@ import com.jongber.game.core.GameLayer;
 import com.jongber.game.core.GameObject;
 import com.jongber.game.core.event.GameEvent;
 
-public class DelRoomEvent extends GameEvent {
+public class DelObjectEvent extends GameEvent {
 
     GameLayer layer;
     GameObject object;
 
-    public DelRoomEvent(GameLayer layer, GameObject object) {
+    public DelObjectEvent(GameLayer layer, GameObject object) {
         this.layer = layer;
         this.object = object;
     }
