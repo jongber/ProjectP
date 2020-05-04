98f201f21ec5e9cbc44163a364dc1cc1668be0dd
JongBeom An <jongber@gmail.com>
2020-05-04 23:22:45 +0900

    코드 정리 및 새 클래스 추가

--- Game2/desktop/src/com/jongber/game/desktop/editor/Inflater.java
+++ Game2/desktop/src/com/jongber/game/desktop/editor/Inflater.java
@@ -66,7 +66,7 @@ public class Inflater {
                 Gdx.app.exit();
             }
         });
-        
+
         cmd.setVisible(true);
     }
 
---
+++ Game2/desktop/src/com/jongber/game/desktop/editor/character/CharacterCmd.java
@@ -0,0 +1,11 @@
+package com.jongber.game.desktop.editor.character;
+
+import com.jongber.game.core.GameLayer;
+import com.jongber.game.desktop.editor.EditorCmd;
+
+public class CharacterCmd extends EditorCmd {
+    @Override
+    public void create(GameLayer layer) {
+        
+    }
+}
