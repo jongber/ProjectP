7dc52349c9810486284182d918cf0fd1dcf3b461
JongBeom An <jongber@gmail.com>
2020-05-05 21:17:17 +0900

    sprite component 코드 작성중

--- Game2/desktop/src/com/jongber/game/desktop/common/component/SpriteComponent.java
+++ Game2/desktop/src/com/jongber/game/desktop/common/component/SpriteComponent.java
@@ -1,6 +1,13 @@
 package com.jongber.game.desktop.common.component;
 
+import com.jongber.game.core.asset.AnimationAsset;
 import com.jongber.game.core.component.Component;
+import com.jongber.game.core.graphics.VFAnimation;
 
 public class SpriteComponent extends Component {
+    public VFAnimation animation = new VFAnimation();
+
+    public void set(AnimationAsset asset, VFAnimation.PlayMode mode) {
+        this.animation.set(asset, mode);
+    }
 }
