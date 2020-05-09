82d9953f1408a2a0678f8869b5e35cab9a4d461e
jongber <jongber@gmail.com>
2020-05-09 14:19:41 +0900

    리소스 정리 및 애니메이션 에디터 작성중..

---
+++ Game2/desktop/src/com/jongber/game/desktop/editor/aimation/AnimationView.java
@@ -0,0 +1,12 @@
+package com.jongber.game.desktop.editor.aimation;
+
+import com.jongber.game.desktop.editor.EditorCmd;
+import com.jongber.game.desktop.editor.EditorView;
+
+public class AnimationView extends EditorView {
+
+    @Override
+    public void create(EditorCmd cmd) {
+
+    }
+}
