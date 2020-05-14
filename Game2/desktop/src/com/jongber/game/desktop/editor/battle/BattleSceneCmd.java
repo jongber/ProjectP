fcb8635c0483c9ec785a4f5e1a6302e999c88e84
JongBeom An <jongber@gmail.com>
2020-05-14 23:13:33 +0900

    전투 씬 구성..

---
+++ Game2/desktop/src/com/jongber/game/desktop/editor/battle/BattleSceneCmd.java
@@ -0,0 +1,11 @@
+package com.jongber.game.desktop.editor.battle;
+
+import com.jongber.game.desktop.editor.EditorCmd;
+import com.jongber.game.desktop.editor.EditorView;
+
+public class BattleSceneCmd extends EditorCmd {
+    @Override
+    public void create(EditorView layer) {
+
+    }
+}
---
+++ Game2/desktop/src/com/jongber/game/desktop/editor/battle/BattleSceneView.java
@@ -0,0 +1,11 @@
+package com.jongber.game.desktop.editor.battle;
+
+import com.jongber.game.desktop.editor.EditorCmd;
+import com.jongber.game.desktop.editor.EditorView;
+
+public class BattleSceneView extends EditorView {
+    @Override
+    public void create(EditorCmd cmd) {
+
+    }
+}
