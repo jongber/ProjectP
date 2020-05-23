640d82e74334c21e1245a31707b575456cc18003
JongBeom An <jongber@gmail.com>
2020-05-23 21:21:49 +0900

    Create DialogController.java

---
+++ Game2/desktop/src/com/jongber/game/desktop/common/controller/game/DialogController.java
@@ -0,0 +1,10 @@
+package com.jongber.game.desktop.common.controller.game;
+
+import com.jongber.game.core.controller.Controller;
+
+public class DialogController extends Controller {
+    @Override
+    public void dispose() {
+
+    }
+}
