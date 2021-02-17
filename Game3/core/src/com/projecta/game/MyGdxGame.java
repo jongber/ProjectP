af375bfbbebb0b3420badbbcc88d505af2787b45
JongBeom An <jongber@gmail.com>
2021-02-17 15:39:26 +0900

    New Game

--- Game3/core/src/com/projecta/game/MyGdxGame.java
+++ Game3/core/src/com/projecta/game/MyGdxGame.java
@@ -1,13 +1,11 @@
-package com.jongber.game;
+package com.projecta.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MainApp extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -19,8 +17,7 @@ public class MainApp extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
