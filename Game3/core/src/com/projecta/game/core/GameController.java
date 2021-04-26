e2dc3940ef486d4a4ed350134656d5a96a8a5738
JongBeom An <jongber@gmail.com>
2021-04-26 21:53:27 +0900

    코드 재작성중..

---
+++ Game3/core/src/com/projecta/game/core/GameController.java
@@ -0,0 +1,4 @@
+package com.projecta.game.core;
+
+public class GameController {
+}
--- Game3/core/src/com/projecta/game/core/GameLayer.java
+++ Game3/core/src/com/projecta/game/core/GameLayer.java
@@ -1,14 +1,7 @@
 package com.projecta.game.core;
 
-import com.projecta.game.core.systems.GameSystem;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameLayer {
-
-    private List<GameSystem> systems = new ArrayList<>();
-
-    public void update(float elapsed) {
-    }
 }
