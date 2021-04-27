dbafdfe35d7664522721651662a19f684522d260
JongBeom An <jongber@gmail.com>
2021-04-27 14:47:42 +0900

    리플렉션 해봄

--- Game3/core/src/com/projecta/game/core/base/GameObject.java
+++ Game3/core/src/com/projecta/game/core/base/GameObject.java
@@ -1,4 +1,28 @@
 package com.projecta.game.core.base;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 public class GameObject {
+    private HashMap<String, GameObjectComponent> componentHashMap = new HashMap<>();
+
+    public void addComponent(Class type, GameObjectComponent c) {
+        this.componentHashMap.put(type.getName(), c);
+    }
+
+    public void bindComponent() {
+        Field[] fields = this.getClass().getFields();
+        for (Field field : fields) {
+            String name = field.getType().getName();
+            if (this.componentHashMap.containsKey(name)) {
+                field.setAccessible(true);
+                try {
+                    field.set(this, this.componentHashMap.get(name));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                field.setAccessible(false);
+            }
+        }
+    }
 }
