b8dc6e708cf4473e60722593dd70d2ef37cd2b61
JongBeom An <jongber@gmail.com>
2021-04-30 19:00:02 +0900

    블록그리드 렌더 작성중

--- Game3/desktop/src/com/projecta/game/desktop/editor/EditorApp.java
+++ Game3/desktop/src/com/projecta/game/desktop/editor/EditorApp.java
@@ -1,12 +1,18 @@
 package com.projecta.game.desktop.editor;
 
 import com.projecta.game.core.GameLayerAppAdapter;
+import com.projecta.game.core.base.layer.GameLayer;
 
 public class EditorApp extends GameLayerAppAdapter {
 
     @Override
     public void create() {
         super.create();
+
+        GameLayer layer = new GameLayer();
+        layer.addPipeline(new BlockGridRender());
+
+        this.addLayer(layer);
     }
 
     @Override
