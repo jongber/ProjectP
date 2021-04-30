5dc3e22859fd0e2e4b10adf09ff4fda06830725c
JongBeom An <jongber@gmail.com>
2021-04-30 20:28:57 +0900

    작성중

--- Game3/core/src/com/projecta/game/core/GameLayerAppAdapter.java
+++ Game3/core/src/com/projecta/game/core/GameLayerAppAdapter.java
@@ -2,13 +2,14 @@ package com.projecta.game.core;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.projecta.game.core.base.layer.GameLayer;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class GameLayerAppAdapter extends ApplicationAdapter {
+public abstract class GameLayerAppAdapter extends ApplicationAdapter implements InputProcessor {
 
     private List<GameLayer> layers = new ArrayList<>();
 
@@ -35,5 +36,86 @@ public abstract class GameLayerAppAdapter extends ApplicationAdapter {
         for (GameLayer layer : this.layers) {
             layer.update(elapsed);
         }
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        for (GameLayer layer : this.layers) {
+            if (layer.keyDown(keycode)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        for (GameLayer layer : this.layers) {
+            if (layer.keyUp(keycode)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        for (GameLayer layer : this.layers) {
+            if (layer.keyTyped(character)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        for (GameLayer layer : this.layers) {
+            if (layer.touchDown(screenX, screenY, pointer, button)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        for (GameLayer layer : this.layers) {
+            if (layer.touchUp(screenX, screenY, pointer, button)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        for (GameLayer layer : this.layers) {
+            if (layer.touchDragged(screenX, screenY, pointer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        for (GameLayer layer : this.layers) {
+            if (layer.mouseMoved(screenX, screenY)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        for (GameLayer layer : this.layers) {
+            if (layer.scrolled(amountX, amountY)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
