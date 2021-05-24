29a96a9486c7c59980271f7b8c4ffdee6f2b6d45
JongBeom An <jongber@gmail.com>
2021-05-24 10:32:54 +0900

    테스트 코드..

--- Game3/desktop/src/com/projecta/game/desktop/editor/panel/SpriteEditorHUDPanel.java
+++ Game3/desktop/src/com/projecta/game/desktop/editor/panel/SpriteEditorHUDPanel.java
@@ -2,7 +2,11 @@ package com.projecta.game.desktop.editor.panel;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.projecta.game.desktop.common.Env;
 import com.projecta.game.desktop.common.panel.HUDPanel;
 import com.projecta.game.desktop.common.pipeline.PerfRender;
 
@@ -17,8 +21,17 @@ public class SpriteEditorHUDPanel extends HUDPanel {
     private void init() {
         this.addPipeline(new PerfRender());
 
-        FileHandle f = Gdx.files.internal("skin/holo/uiskin.json");
+        this.skin = new Skin(Gdx.files.internal("skin/holo/uiskin.json"));
 
-        this.skin = new Skin(f);
+        Button button2 = new TextButton("Text Button", this.skin, "colored");
+        button2.setPosition(100, 100);
+
+        this.stage.addActor(button2);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        this.skin.dispose();
     }
 }
