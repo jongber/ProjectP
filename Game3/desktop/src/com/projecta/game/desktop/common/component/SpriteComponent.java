e160c9e0ae51f73f06aaffb9af9ce0b5e735a986
JongBeom An <jongber@gmail.com>
2021-05-28 08:14:54 +0900

    코드 정리중..

--- Game3/desktop/src/com/projecta/game/desktop/common/component/SpriteComponent.java
+++ Game3/desktop/src/com/projecta/game/desktop/common/component/SpriteComponent.java
@@ -1,15 +1,55 @@
 package com.projecta.game.desktop.common.component;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.projecta.game.core.base.object.GameObjectComponent;
 import com.projecta.game.core.util.Tuple2;
 
 import java.util.ArrayList;
 
 public class SpriteComponent extends GameObjectComponent {
+    public Vector2 pivot;
     public ArrayList<Tuple2<TextureRegion, Integer>> frames = new ArrayList<>();
     public int currentFrame = 0;
     public int elapsedTime = 0;
+
+    public TextureRegion getNext(float elapsed, boolean loop) {
+        int frameLength = this.frames.size();
+        if (frameLength <= 0) {
+            return null;
+        }
+
+        this.elapsedTime += elapsed * 1000.0f;
+
+        Tuple2<TextureRegion, Integer> frame = this.frames.get(this.currentFrame);
+        int curDuration = frame.getItem2();
+        while (this.elapsedTime > curDuration) {
+            this.elapsedTime -= curDuration;
+            this.currentFrame++;
+
+            if (this.currentFrame >= frameLength) {
+                if (loop) {
+                    this.currentFrame = 0;
+                }
+                else {
+                    this.currentFrame--;
+                }
+            }
+
+            frame = this.frames.get(this.currentFrame);
+            curDuration = frame.getItem2();
+        }
+
+        return frame.getItem1();
+    }
+
+    public TextureRegion getCurrent() {
+        return this.frames.get(this.currentFrame).getItem1();
+    }
+
+    public void setPivot(Vector2 pivot) {
+        this.pivot = new Vector2(pivot);
+    }
 
     public void addFrame(TextureRegion r, int frameInterval) {
         this.frames.add(new Tuple2<>(r, frameInterval));
--- Game3/desktop/src/com/projecta/game/desktop/editor/spriteeditor/panel/HUDSpriteEditPanel.java
+++ Game3/desktop/src/com/projecta/game/desktop/editor/spriteeditor/panel/HUDSpriteEditPanel.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.projecta.game.desktop.common.Env;
+import com.projecta.game.desktop.common.component.SpriteComponent;
+import com.projecta.game.desktop.common.component.TextureComponent;
 import com.projecta.game.desktop.common.panel.HUDPanel;
 import com.projecta.game.desktop.common.pipeline.PerfRender;
+import com.projecta.game.desktop.editor.object.SpriteObject;
+import com.projecta.game.desktop.editor.object.TextureObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,15 +30,51 @@ public class HUDSpriteEditPanel extends HUDPanel {
         this.init();
     }
 
-    public void createSprite(String imgPath, Vector2 pivot, int pixelUnitX, int pixelUnitY, int from, int to, int frameRate) {
+    public void createSprite(String imgPath, Vector2 pivot, int pixelUnitX, int pixelUnitY, int from, int to, int frameInterval) {
+
+        this.regionPanel.removeAllObjects();
+        this.animatePanel.removeAllObjects();
 
         Env.assetManager.load(imgPath, Texture.class);
         Env.assetManager.finishLoading();
         Texture t = Env.assetManager.get(imgPath);
 
         List<TextureRegion> regions = new ArrayList<>();
-        int xIndex = t.getWidth() / pixelUnitX;
-        int yIndex = t.getHeight() / pixelUnitY;
+
+        int xUnit = t.getWidth() / pixelUnitX;
+        for (int i = from; i <= to; ++i) {
+            int x = (i % xUnit) * pixelUnitX;
+            int y = t.getHeight() - (i / xUnit + 1) * pixelUnitY;
+
+            TextureRegion r = new TextureRegion(t, x, y, pixelUnitX, pixelUnitY);
+            regions.add(r);
+        }
+
+        // sprite put
+        SpriteComponent c = new SpriteComponent();
+        c.setPivot(pivot);
+        for (TextureRegion r : regions) {
+            c.addFrame(r, frameInterval);
+        }
+        this.putSprite(c);
+
+        // texture put
+        TextureComponent tc = new TextureComponent(new TextureRegion(t));
+        this.putTexture(tc);
+    }
+
+    private void putSprite(SpriteComponent c) {
+        SpriteObject so = new SpriteObject();
+        so.addComponent(SpriteComponent.class, c);
+
+        this.animatePanel.addObject(so);
+    }
+
+    private void putTexture(TextureComponent c) {
+        TextureObject o = new TextureObject();
+        o.addComponent(TextureComponent.class, c);
+
+        this.regionPanel.addObject(o);
     }
 
 //    private void init() {
