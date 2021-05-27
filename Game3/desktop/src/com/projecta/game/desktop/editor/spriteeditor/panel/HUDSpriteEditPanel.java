d33a0bdd419cedb291149c382193c9b090b680a0
JongBeom An <jongber@gmail.com>
2021-05-27 10:18:38 +0900

    코드 정리

--- Game3/desktop/src/com/projecta/game/desktop/editor/spriteeditor/panel/HUDSpriteEditPanel.java
+++ Game3/desktop/src/com/projecta/game/desktop/editor/spriteeditor/panel/HUDSpriteEditPanel.java
@@ -1,12 +1,7 @@
 package com.projecta.game.desktop.editor.spriteeditor.panel;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.projecta.game.core.base.layer.GameLayer;
 import com.projecta.game.core.base.layer.GameLayerMessage;
 import com.projecta.game.desktop.common.Adjuster;
@@ -43,145 +38,72 @@ public class HUDSpriteEditPanel extends HUDPanel {
 
     private void init() {
         this.addPipeline(new PerfRender());
-
-        this.skin = new Skin(Gdx.files.internal("skin/holo/uiskin.json"));
-
-        int width = Gdx.graphics.getWidth();
-        int height = Gdx.graphics.getHeight();
-
-        int widthSum = 0;
-
-        Button button = new TextButton("Create", this.skin, "colored");
-        button.setPosition(width * 0.005f, height - height * 0.1f);
-        button.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                HUDSpriteEditPanel.this.createClicked();
-            }
-        });
-        this.stage.addActor(button);
-
-        widthSum += button.getWidth();
-
-        button = new TextButton("Load", this.skin, "colored");
-        button.setPosition(width * 0.005f + widthSum, height - height * 0.1f);
-        button.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                HUDSpriteEditPanel.this.loadClicked();
-            }
-        });
-        this.stage.addActor(button);
-
-        widthSum += button.getWidth();
-
-        button = new TextButton("Save", this.skin, "colored");
-        button.setPosition( width * 0.005f + widthSum, height - height * 0.1f);
-        button.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                HUDSpriteEditPanel.this.saveClicked();
-            }
-        });
-        this.stage.addActor(button);
-
-        widthSum += button.getWidth();
-
-        button = new TextButton("Clear", this.skin, "colored");
-        button.setPosition(width * 0.005f + widthSum, height - height * 0.1f);
-        button.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                HUDSpriteEditPanel.this.clearClicked();
-            }
-        });
-        this.stage.addActor(button);
     }
+
+//    private void init() {
+//        this.addPipeline(new PerfRender());
+//
+//        this.skin = new Skin(Gdx.files.internal("skin/holo/uiskin.json"));
+//
+//        int width = Gdx.graphics.getWidth();
+//        int height = Gdx.graphics.getHeight();
+//
+//        int widthSum = 0;
+//
+//        Button button = new TextButton("Create", this.skin, "colored");
+//        button.setPosition(width * 0.005f, height - height * 0.1f);
+//        button.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                HUDSpriteEditPanel.this.createClicked();
+//            }
+//        });
+//        this.stage.addActor(button);
+//
+//        widthSum += button.getWidth();
+//
+//        button = new TextButton("Load", this.skin, "colored");
+//        button.setPosition(width * 0.005f + widthSum, height - height * 0.1f);
+//        button.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                HUDSpriteEditPanel.this.loadClicked();
+//            }
+//        });
+//        this.stage.addActor(button);
+//
+//        widthSum += button.getWidth();
+//
+//        button = new TextButton("Save", this.skin, "colored");
+//        button.setPosition( width * 0.005f + widthSum, height - height * 0.1f);
+//        button.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                HUDSpriteEditPanel.this.saveClicked();
+//            }
+//        });
+//        this.stage.addActor(button);
+//
+//        widthSum += button.getWidth();
+//
+//        button = new TextButton("Clear", this.skin, "colored");
+//        button.setPosition(width * 0.005f + widthSum, height - height * 0.1f);
+//        button.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                HUDSpriteEditPanel.this.clearClicked();
+//            }
+//        });
+//        this.stage.addActor(button);
+//    }
 
     @Override
     public void dispose() {
         super.dispose();
         this.skin.dispose();
-    }
-
-    private void createClicked() {
-
-        if (jfcRunning.compareAndSet(false, true)) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        JFileChooser jfc = new JFileChooser();
-                        jfc.addChoosableFileFilter(new FileFilter() {
-                            @Override
-                            public boolean accept(File f) {
-                                if (f.isDirectory()) {
-                                    return true;
-                                } else {
-                                    String name = f.getName().toLowerCase();
-                                    return name.endsWith(".jpg") || name.endsWith("png") || name.endsWith("jpeg");
-                                }
-                            }
-
-                            @Override
-                            public String getDescription() {
-                                return "Image files : jpg, png";
-                            }
-                        });
-                        jfc.setAcceptAllFileFilterUsed(true);
-                        jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
-                        int result = jfc.showOpenDialog(null);
-                        if (result != JFileChooser.APPROVE_OPTION) {
-                            return;
-                        }
-
-                        File f = jfc.getSelectedFile();
-                        String name = f.getName().toLowerCase();
-                        if (!(name.endsWith(".jpg") || name.endsWith("png") || name.endsWith("jpeg"))) {
-                            JOptionPane.showMessageDialog(null, "must be image file!!");
-                            return;
-                        }
-
-                        HUDSpriteEditPanel.this.postMessage(new GameLayerMessage() {
-                            @Override
-                            public void handle(GameLayer layer) {
-                                HUDSpriteEditPanel.this.handleCreate(f);
-                            }
-                        });
-                    }
-                    finally {
-                        jfcRunning.set(false);
-                    }
-                }
-            }).start();
-        }
-    }
-
-    private void handleCreate(File file) {
-        if (this.loadedFileName != null && this.loadedFileName.isEmpty() == false) {
-            Env.assetManager.unload(this.loadedFileName);
-        }
-
-        this.loadedFileName = Adjuster.adjustFilePath(file.getAbsolutePath());
-        Env.assetManager.load(this.loadedFileName, Texture.class);
-        Env.assetManager.finishLoading();
-
-        Texture t = Env.assetManager.get(this.loadedFileName);
-
-        this.regionPanel.onLoadTexture(t);
-    }
-
-    private void loadClicked() {
-    }
-
-    private void saveClicked() {
-    }
-
-    private void clearClicked() {
     }
 }
